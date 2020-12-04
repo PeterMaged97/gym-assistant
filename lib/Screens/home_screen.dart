d2cef4f0e8daf51e54fd0af2a16110e26b56b9a2
Peter <petermounir97@gmail.com>
2020-12-04 20:37:37 +0200

    Added some dependencies & components

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:expandable/expandable.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      child: Scaffold(
+        body: Container(
+          color: Colors.black,
+          child: SafeArea(
+            child: Column(children: [
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 }
