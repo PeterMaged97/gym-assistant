bfea6099f9e946c2d41e5d6fbdc717b55cf15239
Peter <petermounir97@gmail.com>
2020-12-07 20:10:27 +0200

    Designed Skeleton of Home Screen

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:expandable/expandable.dart';
+import 'package:flutter/services.dart';
+import 'package:get_ripped/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Scaffold(
-        body: Container(
-          color: Colors.black,
-          child: SafeArea(
-            child: Column(children: [
-            ]),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [kPrimaryBackgroundColor, kSecondaryBackgroundColor],
           ),
+        ),
+        //color: Colors.black,
+        child: SafeArea(
+          child: Column(
+              children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Text(
+                    'GET RIPPED',
+                    //textAlign: TextAlign.center,
+                    style: TextStyle(
+                      letterSpacing: 10,
+                      color: Color(0x60FFFFFF),
+                      fontSize: 30,
+                    ),
+                  ),
+                  Text(
+                    'GET FIT',
+                    //textAlign: TextAlign.center,
+                    style: TextStyle(
+                      letterSpacing: 4,
+                      color: Color(0x60FFFFFF),
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ]),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_ripped/Screens/home_screen.dart';
 
 void main() {
@@ -8,6 +9,8 @@ void main() {
 class GetRipped extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(systemNavigationBarColor: Color(0x00000000)));
     return MaterialApp(
       initialRoute: '/',
       routes: {
