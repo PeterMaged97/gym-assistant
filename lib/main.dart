20ce83dc4ebc2e3f54ba81e4c1377adb6cbc4e5c
Peter <petermounir97@gmail.com>
2020-12-11 21:42:38 +0200

    wrote the function responsible for loading the user data

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,47 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_ripped/Screens/home_screen.dart';
 import 'package:get_ripped/constants.dart';
+import 'package:get_ripped/day.dart';
 import 'package:get_ripped/exercise.dart';
+import 'package:get_ripped/routine.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(GetRipped());
 }
 
-void loadData() async {
+Future<List<Routine>> loadData() async {
   final prefs = await SharedPreferences.getInstance();
+
+  List<Exercise> exercises = [];
+  List<Day> days = [];
+  List<Routine> routines = [];
+
   for (String exerciseName in kExerciseNames){
     int sets = prefs.getInt('$exerciseName sets') ?? 0;
     int reps = prefs.getInt('$exerciseName reps') ?? 0;
     int weight = prefs.getInt('$exerciseName weight') ?? 0;
     bool isMetric  = prefs.getBool('$exerciseName isMetric') ?? true;
 
-    Exercise e = Exercise(exerciseName.toUpperCase(), sets, reps, weight, isMetric);
-
+    exercises.add(Exercise(exerciseName.toUpperCase(), sets, reps, weight, isMetric));
   }
-  //ToDo: Loop over all exercise names, load 'sets', 'reps', and 'weight' (put a default 0 if they don't exist) and initialize objects for them.
-  //ToDo: after loading all exercises, create days containing list of exercises and routines containing list of days.
+  kDayNames.forEach((key, value) {
+    List<Exercise> e = [];
+    for (int i in value) {
+      e.add(exercises[i]);
+    }
+    days.add(Day(key, e));
+  });
+
+  kRoutineNames.forEach((key, value) {
+    List<Day> d = [];
+    for (int i in value) {
+      d.add(days[i]);
+    }
+    routines.add(Routine(key, d));
+  });
+  return routines;
 }
 
 class GetRipped extends StatelessWidget {
