ac868bbcdeb84edbbc610556a5fe6ac9a9c70300
Peter <petermounir97@gmail.com>
2020-12-11 21:04:36 +0200

    updated constants to better represent the exercise hierarchy

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,13 +5,14 @@ final Color kSecondaryBackgroundColor = Color(0xFF292E38);
 final Color kPrimaryColor = Color(0x60FFFFFF);
 final Color kTertiaryBackgroundColor = Color(0xFF18181D);
 
-final List<String> exerciseNames = [
+final List<String> kExerciseNames = [
   'dumbbell flat bench press',
   'dumbbell inclined bench press',
   'chest fly (machine)',
   'dips',
   'unilateral dumbbell french',
   'cable triceps extensions',
+  'shoulder press',
   'pulldown',
   'low row',
   'row (machine)',
@@ -29,22 +30,22 @@ final List<String> exerciseNames = [
   'side laterals',
 ];
 
-final List<String> dayNames = [
-  'Push',
-  'Pull',
-  'Legs',
-  'Chest',
-  'Back',
-  'Shoulders',
-  'Arms',
-  'Upper Body',
-  'Lower Body',
-  'General',
-];
+final Map<String, List<int>> kDayNames = {
+  'Push': [0,1,2,3,4,5,6,20,21],
+  'Pull': [7,8,9,10,11,12,13,14],
+  'Legs': [15,16,17,18,19],
+  'Chest': [0,1,2,3],
+  'Back': [7,8,9,10,11],
+  'Shoulders': [6,12,20,21],
+  'Arms': [4,5,13,14],
+  'Upper Body': [0,1,2,4,6,7,8,12,13,14,21],
+  'Lower Body': [15,16,17,18,19,11],
+  'General': [0,1,3,4,6,7,8,12,13,15,19,21],
+};
 
-final List<String> routineNames = [
-  'PPL',
-  'Bro Split',
-  'Upper Lower',
-  'General',
-];
+final  Map<String, List<int>> kRoutineNames = {
+  'PPL': [0, 1, 2],
+  'Bro Split': [3,4,5,6,2],
+  'Upper Lower': [7,8],
+  'General': [9],
+};
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_ripped/Screens/home_screen.dart';
+import 'package:get_ripped/constants.dart';
+import 'package:get_ripped/exercise.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(GetRipped());
 }
 
-void loadData(){
+void loadData() async {
+  final prefs = await SharedPreferences.getInstance();
+  for (String exerciseName in kExerciseNames){
+    int sets = prefs.getInt('$exerciseName sets') ?? 0;
+    int reps = prefs.getInt('$exerciseName reps') ?? 0;
+    int weight = prefs.getInt('$exerciseName weight') ?? 0;
+    bool isMetric  = prefs.getBool('$exerciseName isMetric') ?? true;
+
+    Exercise e = Exercise(exerciseName.toUpperCase(), sets, reps, weight, isMetric);
+
+  }
   //ToDo: Loop over all exercise names, load 'sets', 'reps', and 'weight' (put a default 0 if they don't exist) and initialize objects for them.
   //ToDo: after loading all exercises, create days containing list of exercises and routines containing list of days.
 }
